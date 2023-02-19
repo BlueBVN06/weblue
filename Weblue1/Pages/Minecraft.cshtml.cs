5489467e0058a387e4d2daaae656fceea4561c64
administrator <administrator@H>
2023-02-19 10:25:02 +0100

    Add project files.

---
+++ Weblue1/Pages/Minecraft.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Weblue1.Pages
+{
+    public class MinecraftModel : PageModel
+    {
+        private readonly ILogger<MinecraftModel> _logger;
+
+		public MinecraftModel(ILogger<MinecraftModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        
+        }
+    }
+}
